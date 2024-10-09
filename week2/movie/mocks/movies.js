07d38efa636140c2d7eb03fc342de309449475b6
jiwon234 <145983750+jiwon234@users.noreply.github.com>
2024-10-09 16:12:27 +0900

    week3 영화 api 실습

---
+++ week2/movie/mocks/movies.js
@@ -0,0 +1,27 @@
+export const MOVIES = {
+  "dates": {
+    "maximum": "2023-09-17",
+    "minimum": "2023-07-31"
+  },
+  "page": 1,
+  "results": [
+    {
+      "adult": false,
+      "backdrop_path": "/8pjWz2lt29KyVGoq1mXYu6Br7dE.jpg",
+      "genre_ids": [28, 12, 16],  // 예시로 장르 ID 배열을 추가
+      "id": 123456,
+      "original_language": "en",
+      "original_title": "Example Movie Title",
+      "overview": "This is a brief description of the movie.",
+      "popularity": 10.5,
+      "poster_path": "/examplePoster.jpg",
+      "release_date": "2023-08-15",
+      "title": "Example Movie",
+      "video": false,
+      "vote_average": 7.8,
+      "vote_count": 1234
+    }
+  ],
+  "total_pages": 5,
+  "total_results": 100
+};
--- week2/movie/src/App.jsx
+++ week2/movie/src/App.jsx
@@ -1,13 +1,31 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 import React from "react";
-import { MOVIES } from "../api/movies";
+import { MOVIES } from "../mocks/movies";
 import './App.css';
 
 const App = () => {
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const getMovies = async () => {
+        const movies = await axios.get(`https://api.themoviedb.org/3/movie/popular?language=en-US&page=1`, {
+            headers: {
+                Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNTNkYWIyMDkxMzI2Y2Y3NTkwNTAwYjQyODNkNjZkNyIsIm5iZiI6MTcyNjE0MTU3Ny42MDM2ODcsInN1YiI6IjY0MzVmY2Y2NjUxZmNmMDBkM2RhYzNmNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.cFPsPRHPidq2OnJ3U-3wHJYhnGajDFqUsM8XJ_a_0bw`,
+            }
+        })
+        setMovies(movies);
+    }
+    getMovies()
+}, []);
+
   return (
     <div>
     <h1>Movie List</h1>
     <div style={{ display: 'flex', flexDirection: 'row', flexWrap:'wrap'}}>
-      {MOVIES.results.map((movie) => (
+      {movies.data?.results.map((movie) => (
         <div key={movie.id} className='movie-item'>
           <img className='movie-poster' src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
         </div>
