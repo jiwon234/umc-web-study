640e864a1bdb4ba6b3666b58450a8a011670bec8
jiwon234 <145983750+jiwon234@users.noreply.github.com>
2024-11-15 12:29:09 +0900

    Chap7 미션3: Pagination 기능 구현
    up-coming 페이지만 Pagination을 적용해 보았습니다.

--- week3/youngcha/frontend/src/pages/MoviePages/up-coming.jsx
+++ week3/youngcha/frontend/src/pages/MoviePages/up-coming.jsx
@@ -1,36 +1,86 @@
-import React from "react";
+import React, { useEffect } from "react";
 import useCustomFetch from "../../hooks/useCustomFetch";
-import MovieItem from "../../components/Card/Card";
-import * as S from "../search/search.style";
+import MovieItem from "../../components/MovieItem/MovieItem";
+import * as S from "../Search/search.style";
+import { useGetMovies } from "../../hooks/queries/useGetMovies";
+import { useQuery } from "@tanstack/react-query";
+import CardListSkeleton from "../../components/Skeleton/card-list-skeleton";
+import { useGetInfiniteMovies } from "../../hooks/useGetInfiniteMovies";
+import {useInView} from "react-intersection-observer";
+import ClipLoader from "react-spinners/ClipLoader";
+import { useState } from "react";
+import './movie-pagenation.css';
+import { FaAngleRight, FaAngleLeft } from "react-icons/fa6";
+import { axiosInstance } from "../../apis/axios-instance";
+
 
 const UpComing = () => {
-  const {data:movies , isLoading, isError} = useCustomFetch('/movie/upcoming?language=ko-KR&page=1');
-  
-  if (isLoading) {
-    return (
-      <div>
-        <h1 style={{color: "white"}}>로딩 중 입니다...</h1>
-      </div>
-    )
-  }
 
-  if (isError) {
-    return (
-      <div>
-        <h1 style={{color: "white"}}>에러 중...</h1>
-      </div>
-    )
+  const [page, setPage] = useState(1);
+
+  const useGetUpComingMovies = async (page) => {
+    const data = await axiosInstance.get(`/movie/upcoming?language=ko-KR&page=${page}`);
+    console.log(data);
+    return data;
   }
+  const {
+    isLoading,
+    isError,
+    error,
+    data,
+    isFetching,
+    isPreviousData,
+  } = useQuery({
+    queryKey: ['movies', page],
+    queryFn: () => useGetUpComingMovies(page),
+    keepPreviousData: true,
+  });
+
+  
   return (
+    
     <div>
-      <h1>개봉 예정중인</h1>
-      <S.MovieGridContainer>
-        {movies.data?.results.map((movie) => (
-          <MovieItem key={movie.id} movie={movie} />
-        ))}
-      </S.MovieGridContainer>
-  </div>
-  );
-};
+      <h1>개봉 예정 중인</h1>
+      {isLoading ? (
+        <S.MovieGridContainer>
+          <CardListSkeleton number={20} />
+        </S.MovieGridContainer>
+         
+      ) : isError ? (
+        <div>Error: {error.message}</div>
+      ) : (
+        <S.MovieGridContainer>
+          {data?.data?.results?.map((movie) => {
+            return <MovieItem movie={movie} key={movie.id} />
+          })
+          }
+        </S.MovieGridContainer>
+      )}
+      <div style={{display: 'flex', gap: '30px', justifyContent: 'center', width:'100%', marginTop: '30px'}}>
+      <button className='page-btn'
+        onClick={() => setPage(old => Math.max(old - 1, 0))}
+        disabled={page === 1}
+      >
+        <FaAngleLeft />
+      </button>
+      <span>{page} 페이지</span>
+      <button className='page-btn'
+        onClick={() => {
+          if (!isPreviousData) {
+            setPage(old => old + 1)
+          }
+        }}
+        // Disable the Next Page button until we know a next page is available
+        // disabled={isPreviousData -}
+      >
+        <FaAngleRight/>
+      </button>
+      </div>
+      
+      {isFetching ? <span> Loading...</span> : null}{' '}
+    </div>
+    
+  )
+}
 
 export default UpComing;
